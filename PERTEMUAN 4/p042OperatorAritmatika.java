fd9ce948944eab86b3a4fbc0737d541789e18415
evasaae <evvasfttrii@gmail.com>
2026-02-14 00:36:23 +0700

    first commit

---
+++ PERTEMUAN 4/p042OperatorAritmatika.java
@@ -0,0 +1,24 @@
+/*
+Nim  : D1041241012
+Nama : Eva Safitri
+Program penggunaan operator aritmatika dasar
+ */
+
+public class p042OperatorAritmatika {
+public static void main(String[] args) {
+    //deklarasi variabel
+int a = 10;
+int b = 3;
+System.out.println("a = " + a);
+System.out.println("b = " + b);
+System.out.println();
+System.out.println("a + b = " + (a + b));
+System.out.println("a - b = " + (a - b));
+System.out.println("a * b = " + (a * b));
+System.out.println("a / b = " + (a / b));
+System.out.println("a % b = " + (a % b));
+// Pembagian dengan desimal
+double hasil = (double) a / b;
+System.out.println("a / b (desimal) = " + hasil);
+}
+}
