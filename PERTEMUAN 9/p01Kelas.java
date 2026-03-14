65a820942451a1527fe8e4565f11e47010bd035d
evasaae <evvasfttrii@gmail.com>
2026-03-14 14:53:26 +0700

    update

---
+++ PERTEMUAN 9/p01Kelas.java
@@ -0,0 +1,25 @@
+class Calc {
+    int num1;
+    int num2;
+    int result;
+    public void perform() {
+        result = num1 + num2;
+    }
+}
+
+public class p01Kelas {
+    public static void main(String args[]) {
+        // Cara 1: Deklarasi dan instansiasi terpisah
+        Calc obj; // Deklarasi
+        obj = new Calc(); // Instansiasi
+        
+        // Cara 2: Deklarasi dan instansiasi dalam 1 baris
+        Calc obj1 = new Calc();
+        // Mengisi atribut object
+        obj.num1 = 3;
+        obj.num2 = 5;
+
+        // Menampilkan nilai result sebelum method dipanggil
+        System.out.println(obj.result);
+    }
+}
